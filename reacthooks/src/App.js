ad5b16eae59978db80d78cee70a49e2238dad974
MarinaYsn <marina@bluelink.ca>
2020-11-05 00:38:51 -0500

    intro to useEffect

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
-import ClassCounter from './components/ClassCounter'
-import HookCounter from './components/HookCounter';
-import HookCounter2 from './components/HookCounter2';
-import HookCounter3 from './components/HookCounter3';
-import HookCounter4 from './components/HookCounter4';
+import ClassCounter from './components/useStateHooks/ClassCounter'
+import HookCounter from './components/useStateHooks/HookCounter';
+import HookCounter2 from './components/useStateHooks/HookCounter2';
+import HookCounter3 from './components/useStateHooks/HookCounter3';
+import HookCounter4 from './components/useStateHooks/HookCounter4';
+import ClassCounterOne from './components/useEffect/ClassCounterOne';
+import UseEffectCounter from './components/useEffect/UseEffectCounter';
+
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter />
       <HookCounter />
       <HookCounter2 />
-      <HookCounter3 /> */}
-      <HookCounter4 />
+      <HookCounter3 />
+      <HookCounter4 /> */}
+      
+      {/* <ClassCounterOne /> */}
+
+      <UseEffectCounter />
     </div>
   );
 }
---
+++ reacthooks/src/components/useEffect/ClassCounterOne.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+class ClassCounterOne extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    
+
+    componentDidMount() {
+        document.title = `Clicked ${this.state.count} times`
+    }
+    componentDidUpdate(prevProps, prevState) {
+        document.title = `Clicked ${this.state.count} times` 
+    }
+
+    render() {
+        return (
+            <div>
+                <button onClick={() =>this.setState({count: this.state.count +1})}> Clicked {this.state.count} times</button>
+
+             <div> 
+             <h3>
+                Clicked {this.state.count} times 
+             </h3>
+              </div>   
+            </div>
+        )
+    }
+}
+
+export default ClassCounterOne
