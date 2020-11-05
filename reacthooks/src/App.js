7d6ef69cb0c03e4e9bf870572c5f02d585b6464f
MarinaYsn <marina@bluelink.ca>
2020-11-04 23:50:00 -0500

    useState with array

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -3,13 +3,15 @@ import ClassCounter from './components/ClassCounter'
 import HookCounter from './components/HookCounter';
 import HookCounter2 from './components/HookCounter2';
 import HookCounter3 from './components/HookCounter3';
+import HookCounter4 from './components/HookCounter4';
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter />
       <HookCounter />
-      <HookCounter2 /> */}
-      <HookCounter3 />
+      <HookCounter2 />
+      <HookCounter3 /> */}
+      <HookCounter4 />
     </div>
   );
 }
