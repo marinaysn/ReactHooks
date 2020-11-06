7c2dbbb3aa5a905694d1de90396fc198385c7456
MarinaYsn <marina@bluelink.ca>
2020-11-06 12:54:45 -0500

    useEffect hooks intro

--- reacthooks/src/components/useEffect/UseEffectCounter.js
+++ reacthooks/src/components/useEffect/UseEffectCounter.js
@@ -1,32 +1,38 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 function UseEffectCounter() {
 
-    //const [count, setCount] = useState(0);
-
     const [count, setCount] = useState(0);
-    
+    const [name, setName] = useState('');
+
     useEffect(() => {
-        
+        console.log("Update title")
         document.title = `You Clicked ${count} times`
- 
-    });
+
+    },[count]);
 
 
     return (
         <div>
+            <div>
+
+                <input type='text' value={name}
+                    onChange={ e => setName(e.target.value)
+                    } />
+
+                <p>{name}</p>
+            </div>
+
             <button
-            onClick={() => {setCount(count +1)}}>
+                onClick={() => { setCount(count + 1) }}>
                 Click {count} times
             </button>
 
-            <div> 
-             <h3>
-                Clicked {count} times 
+            <div>
+                <h3>
+                    Clicked {count} times
              </h3>
-              </div>
-
-
+            </div>
         </div>
     )
 }
