9862ac229bf75a858ba0f5e6c79c7f36149cca42
MarinaYsn <marina@bluelink.ca>
2020-11-08 09:13:43 -0500

    useContext from child component down the chain intro

--- reacthooks/src/components/useContext/ComponentE.js
+++ reacthooks/src/components/useContext/ComponentE.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import ComponentF from './ComponentF'
-import ComponentH from './ComponentH'
+import ComponentH from './ComponentH';
+
+export const CarContext = React.createContext();
+export const FoodContext = React.createContext();
+
+
 function ComponentE() {
     return (
         <div>
-           <ComponentF />
-           <ComponentH /> 
+
+    <CarContext.Provider value='Acura'>
+        <FoodContext.Provider value='Fish'>
+          
+          <ComponentF />
+           <ComponentH />
+
+        </FoodContext.Provider>
+      </CarContext.Provider>
+
+
+            
         </div>
     )
 }
