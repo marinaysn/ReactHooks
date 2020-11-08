19593f8cb1024e53731a21f3dd7ab6f991056ead
MarinaYsn <marina@bluelink.ca>
2020-11-08 14:29:13 -0500

    fetching data with useState

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useContext, useEffect, useReducer } from 'react';
 import './App.css';
-
+import DataFetching2 from './components/DataStream/DataFetching2'
 function App() {
 
   return (
       <div className="App">
        
-
+<DataFetching2 />
       </div>
   );
 }
