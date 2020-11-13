ef2360ae870da743544d5926fdb34d8feeaa2a78
MarinaYsn <marina@bluelink.ca>
2020-11-13 00:26:52 -0500

    useRef intro

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,16 +1,14 @@
 import React, { useState, useContext, useEffect, useReducer } from 'react';
 import './App.css';
 import DataFetching2 from './components/DataStream/DataFetching2';
-import ParentComponent from './components/useCallback/ParentComponent';
-
-import CounterUM from './components/useMemo/CounterUM'
+import FocusInput from './components/useRef/FocusInput'
 
 function App() {
 
   return (
     <div className="App">
 
-      <CounterUM />
+      <FocusInput />
 
     </div>
   );
