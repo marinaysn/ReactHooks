1141417e4f9f90644b717157f06c0a5364b51313
MarinaYsn <marina@bluelink.ca>
2020-12-01 23:14:52 -0500

    initial commit for MS Hooks projevt

--- reacthooks/src/components/CustomHooks/CounterOne.js
+++ reacthooks/src/components/CustomHooks/CounterOne.js
@@ -1,14 +1,13 @@
-import React, {useState} from 'react';
+import React from 'react';
 import useCounter from './useCounter'
 
 function CounterOne() {
 
     const [count, increment, decrement, reset] = useCounter(10, 10) 
-
+    console.log(process.env.REACT_APP_URL_MAIN)
     return (
 
         <div>
-
             <h2>{count}</h2>
             <button onClick={increment}> Add</button>
 
